96da04ed1839c99675ee535a691fb67832c4a65d
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-08-10 01:55:39 +0400

    Update SelectExcel.tsx

--- src/component/SelectExcel.tsx
+++ src/component/SelectExcel.tsx
@@ -1,34 +1,69 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-
-export default function BasicSelect(props:any) {
-    const [age, setAge] = React.useState('');
-
-    const handleChange = (event: SelectChangeEvent) => {
-        setAge(event.target.value as string);
-    };
-
-    return (
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={age}
-                    label="Тип сварчного соединения"
-                    onChange={handleChange}
-                    {...props.register('tipsWeldedConnection')}
-                >
-                    <MenuItem value={10}>Тавровое</MenuItem>
-                    <MenuItem value={20}>Нахлесточное </MenuItem>
-                    <MenuItem value={30}>Стыковое</MenuItem>
-                </Select>
-            </FormControl>
-        </Box>
-    );
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import BasicSelect from "./SelectExcel";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Inputs } from "../App";
+import { Button } from "@mui/material";
+export default function BasicTextFields(props: any) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    props.newArrayValue(data);
+  };
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextField
+        {...register("WeldNo")}
+        id="standard-basic"
+        label="Сварочное соединение"
+        variant="standard"
+      />
+      <TextField
+        {...register("wallThickness")}
+        id="standard-basic"
+        label="Диаметр толщина стенки"
+        variant="standard"
+      />
+      <TextField
+        {...register("seamType")}
+        id="standard-basic"
+        label="Тип шва"
+        variant="standard"
+      />
+      <BasicSelect register={register} />
+      <TextField
+        {...register("WeldLength")}
+        id="standard-basic"
+        label="Длина сварного соединения"
+        variant="standard"
+      />
+      <TextField
+        {...register("connectedElements")}
+        id="standard-basic"
+        label="Наименование соединяемых"
+        variant="standard"
+      />
+      <TextField
+        {...register("GradeSteel")}
+        id="standard-basic"
+        label="Марка стали "
+        variant="standard"
+      />
+      <Button type="submit"> добавить строку</Button>
+    </Box>
+  );
 }
